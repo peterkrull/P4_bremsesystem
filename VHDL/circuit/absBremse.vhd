03cfc417bcfe5fd7d4d6a96fcf15a90a39bafabe
JakobGammelgaard <gsenersej@gmail.com>
2021-04-27 15:33:43 +0200

    Fikset motorkraft under 5 km/t i abs-bremse

--- VHDL/circuit/absBremse.vhd
+++ VHDL/circuit/absBremse.vhd
@@ -29,18 +29,18 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity absBremse is
+entity absBrems is
 	port(
 		clk	:	in	std_logic;
 		frontSpeed	:	in	std_logic_vector(7 downto 0);
 		rearSpeed	:	in	std_logic_vector(7 downto 0);
 		engPower	:	out	std_logic_vector(7 downto 0)
 	);
-end absBremse;
+end absBrems;
 
-architecture Behavioral of absBremse is
-	signal speedABS		:	std_logic_vector(15 downto 0)	:= (others => '0');
-	signal engPowerCopy	:	std_logic_vector(7 downto 0):= (others => '0');
+architecture Behavioral of absBrems is
+	signal speedABS		:	std_logic_vector(15 downto 0):="0000000000000000";
+	signal engPowerCopy	:	std_logic_vector(7 downto 0):="00000000";
 
 begin	
 
@@ -48,10 +48,14 @@ begin
 
 process(clk) begin
 	if(clk'event and clk = '1') then
-		if(frontSpeed > speedABS and engPowerCopy < "11111111" and rearSpeed > "00101000") then
-		engPowerCopy <= std_logic_vector(unsigned(engPowerCopy) + "00000001");
-		elsif(frontSpeed < speedABS and engPowerCopy > "00000000" and rearSpeed > "00101000") then
-		engPowerCopy <= std_logic_vector(unsigned(engPowerCopy) - "00000001");
+		if(rearSpeed > "00101000")then
+			if(frontSpeed > speedABS and engPowerCopy < "11111111") then
+			engPowerCopy <= std_logic_vector(unsigned(engPowerCopy) + "00000001");
+			elsif(frontSpeed < speedABS and engPowerCopy > "00000000") then
+			engPowerCopy <= std_logic_vector(unsigned(engPowerCopy) - "00000001");
+			end if;
+		else
+			engPowerCopy <= "00000000";
 		end if;
 	end if;
 end process;
