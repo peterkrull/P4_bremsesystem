d480159ba8572d8ec8b3a3ca9f1ba7e86c8fe0c4
Peter Krull <31969909+peterkrull@users.noreply.github.com>
2021-05-04 23:02:31 +0200

    Opdateret encoder_v2 til at understøtte eksperimentel lokal buffer

--- Arduino/Encoder_v2/Encoder_v2.ino
+++ Arduino/Encoder_v2/Encoder_v2.ino
@@ -2,7 +2,7 @@
 #include <SD.h>
 File myFile;  
 
-#define printing
+//#define printing
 
 // Invert behaviour of braking input
 boolean invert_braking_in = false;
@@ -16,7 +16,7 @@ const int sens_driv_pin = 4;
 const int sens_roll_pin = 5;
 const int braking_pin_in = 3;
 const int braking_pin_out = 9;
-const int led_int_pin = 13;
+const int led_int_pin = A5;
 
 // previous and current states for delta calc
 boolean curr_sens_driv;
@@ -30,7 +30,7 @@ boolean roll_diff_found = false;
 // previous, current and delta times
 unsigned long prev_time_driv, curr_time_driv, delta_time_driv;
 unsigned long prev_time_roll, curr_time_roll, delta_time_roll;
-unsigned long stop_timer, curr_time; 
+unsigned long stop_timer, curr_time, prev_time; 
 
 // state booleans
 boolean driving = false;
@@ -46,8 +46,9 @@ String cache = "cache.txt";
 void setup() {
 
   #ifdef printing
-  Serial.begin(115200);
+  Serial.begin(1000000);
   #endif
+  Serial.begin(500000);
   
   // encoder pins
   pinMode(sens_driv_pin , INPUT);
@@ -73,23 +74,30 @@ void setup() {
   #ifdef printing
   Serial.println("initialization done.");
   #endif
-  
-  /*
-  myFile = SD.open("test.txt", FILE_WRITE);
-  myFile.println("\n# first line of test");
-  #ifdef printing
-  Serial.println("\n# first line of test");
-  #endif
-  */
 
   sd_get_counter();
-  Serial.print("CNTR0 ");Serial.println(test_counter);
 }
+
+boolean loop_bool = true;
+unsigned long loop_timer = 0;
+int counter_flush = 0;
 
 void loop(){
   curr_sens_driv = digitalRead(sens_driv_pin);
   curr_sens_roll = digitalRead(sens_roll_pin);
+
+  prev_time = curr_time;
   curr_time = micros();
+
+  // Detect long loop times by lighting up LED
+  if ( curr_time - prev_time > 1000 && loop_bool) {
+    digitalWrite(led_int_pin,HIGH);
+    loop_timer = curr_time;
+    loop_bool = false;
+  } else if (curr_time > loop_timer + 100e3) { 
+    digitalWrite(led_int_pin,LOW);
+    loop_bool = true;
+  }
    
   // calc delta time for ROLLING wheel
   if (curr_sens_roll != prev_sens_roll){
@@ -97,7 +105,6 @@ void loop(){
     prev_sens_roll = curr_sens_roll;
     delta_time_roll = curr_time_roll - prev_time_roll;
     prev_time_roll = curr_time_roll;
-    //Serial.print("ROLL ");Serial.println(delta_time_roll);
     roll_diff_found = true;
   }
 
@@ -107,7 +114,6 @@ void loop(){
     curr_time_driv = curr_time;
     delta_time_driv = curr_time_driv - prev_time_driv;
     prev_time_driv = curr_time_driv;
-    //Serial.print("DRIV ");Serial.println(delta_time_driv);
     driv_diff_found = true;
   }
 
@@ -116,17 +122,18 @@ void loop(){
 
   // if there is a delta for rolling wheel
   if (roll_diff_found){
-    //if (false){  
+
 
     // Identify long delta times;
-    if (delta_time_roll > slow_time * 1.2 && stop_time_noted == false && driving){ // time higher, drives slower 1.2
-      // define time to stop logging data.
+    if (delta_time_roll > slow_time * 1.2 && stop_time_noted == false && driving){
       stop_timer = micros() + stop_time_delay;
       stop_time_noted = true;
       
-    } else if (delta_time_roll < slow_time * 0.8) { // time lower, drives faster 0.8
+    } else if (delta_time_roll < slow_time * 0.8 && driving == false) { // time lower, drives faster 0.8
       driving = true; 
       sd_init();
+    } else {
+      stop_timer = micros() + 1e6; 
     }
 
     // if driving, print rear data to SD
@@ -155,7 +162,6 @@ void loop(){
 
   // if there is a delta for driving wheel
   if (driv_diff_found){
-    //if (false){
 
     // if driving print front data to SD
     if (driving){
@@ -168,15 +174,16 @@ void loop(){
       #ifdef printing
       Serial.println(str_buf);
       #endif
-
       driv_diff_found = false;
     }
   }
 
-  // print wether stop_State has been noted
-  digitalWrite(braking_pin_out,stop_time_noted);
-  digitalWrite(led_int_pin,stop_time_noted);
-  
+  // print wether car has stopped has been noted
+  if ( delta_time_roll > slow_time * 0.7 ) {
+    digitalWrite(braking_pin_out,true);
+  } else if ( delta_time_roll < slow_time * 0.5 ) {
+    digitalWrite(braking_pin_out,false);
+  }  
 }
 
 // Restarts connection with SD card
@@ -188,7 +195,7 @@ void sd_init(){
 
     sprintf(file_str_buf, "file_%i.txt", test_counter);
 
-    myFile = SD.open(file_str_buf, FILE_WRITE);
+    myFile = SD.open(file_str_buf, O_CREAT | O_WRITE);
     myFile.println("#\t0\t#");
     sd_closed = false;
     first_print = true;
@@ -201,12 +208,9 @@ void sd_close(){
     #ifdef printing
     Serial.println("Closing SD card");
     #endif
-    //myFile.println("X last line of test");
     myFile.close();
     test_counter++;
-    Serial.print("CNTR1 ");Serial.println(test_counter);
-    sd_set_counter();
-    Serial.print("CNTR2 ");Serial.println(test_counter);
+    sd_set_counter();;
     sd_closed = true;
     first_print = false;
   }
@@ -220,15 +224,49 @@ void sd_get_counter(){
     temp_num += (char)myFile.read();
   }
   test_counter = temp_num.toInt();
-  Serial.print("CNTR3 ");Serial.println(temp_num);
-  Serial.print("CNTR4 ");Serial.println(test_counter);
   myFile.close();
 }
 
 // Write test counter to SD card
 void sd_set_counter(){
   SD.remove(cache);
-  myFile = SD.open(cache, FILE_WRITE);
+  myFile = SD.open(cache, O_CREAT | O_WRITE);
   myFile.println(test_counter);
   myFile.close();
+}
+
+const byte buff_size = 50;
+unsigned long time_ready = 0;
+byte temp_count = 0;
+byte buff_count = 0;
+String str_buffer = "";
+
+void sd_smart_buffer(String input_str){
+
+  input_str += "\n";
+
+  // If buffer is not full and time is past ready_time, print normally.
+  if (millis() > time_ready && buff_count < buff_size){
+    // If buffer was just emptied, print local buffer
+    if (temp_count > 0){
+      myFile.print(str_buffer);
+      buff_count += temp_count;
+      temp_count = 0; 
+    }
+    myFile.print(input_str);
+    buff_count++;
+
+  // If buffer is full, initiate local buffer
+  } else if (buff_count == buff_size){
+    SD.flush();
+    buff_count = 0;
+    str_buffer = "";
+    time_ready = millis() + 500;
+  } 
+  
+  // If buffer is still flushing, add to local buffer
+  if (millis() <= time_ready){
+    str_buffer += input_str;
+    temp_count++;
+  }
 }
